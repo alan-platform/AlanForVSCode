ced3407604cdb091f688f4b7136ea984326c3eec
Koen Lageveen <koen.lageveen@me.com>
2018-06-26 01:55:18 -0700

    only show tree for alan files + clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,29 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import {extname, dirname, join} from 'path';
-import {exec} from 'child_process';
 import {fuzzyDefinitionSearch} from './search';
 
 import { AlanTreeViewDataProvider } from './providers/AlanTreeView'
 
 //This extension is based on Fuzzy Definitions from Johannes Rieken
 
+export function deactivate(context: vscode.ExtensionContext) {
+    vscode.commands.executeCommand('setContext', 'isAlanFile', false);
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
     let config = vscode.workspace.getConfiguration('alan-definitions');
     let registrations: vscode.Disposable[] = [];
+
+    function checkState() {
+        if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId == "alan") {
+            vscode.commands.executeCommand('setContext', 'isAlanFile', true);
+        } else {
+            vscode.commands.executeCommand('setContext', 'isAlanFile', false);
+        }
+    }
+    checkState();
 
     registrations.push(vscode.commands.registerTextEditorCommand('editor.gotoAlanDefinitions', editor => {
 
@@ -45,12 +56,38 @@ export function activate(context: vscode.ExtensionContext) {
 
     // pretend to be a definition provider
     if (config.get<boolean>('integrateWithGoToDefinition')) {
-        registrations.push(vscode.languages.registerDefinitionProvider('alan', { provideDefinition: fuzzyDefinitionSearch }));
+        registrations.push(vscode.languages.registerDefinitionProvider(
+            'alan', {
+                provideDefinition: fuzzyDefinitionSearch
+            }
+        ));
     }
 
     context.subscriptions.push(...registrations);
-
-    context.subscriptions.push(vscode.window.registerTreeDataProvider("alanTreeView", new AlanTreeViewDataProvider(context)));
+    context.subscriptions.push(
+        vscode.window.registerTreeDataProvider(
+            "alanTreeView",
+            new AlanTreeViewDataProvider(context)
+        )
+    );
+   
+    context.subscriptions.push(
+        vscode.window.onDidChangeActiveTextEditor(() => {
+            checkState();
+        }, null, context.subscriptions ) 
+    );   
+    context.subscriptions.push(
+        vscode.workspace.onDidOpenTextDocument(() => {
+            checkState();
+        }, null, context.subscriptions ) 
+    );
+    context.subscriptions.push(
+        vscode.workspace.onDidCloseTextDocument(() => {
+            if (vscode.window.visibleTextEditors.length < 1) {
+                vscode.commands.executeCommand('setContext', 'isAlanFile', false);
+            }
+        }, null, context.subscriptions ) 
+    );
 }
 
 function openLocation(location: vscode.Location) {
@@ -60,31 +97,3 @@ function openLocation(location: vscode.Location) {
         });
     });
 }
-
-
-// import * as vscode from 'vscode';
-
-// // this method is called when your extension is activated
-// // your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-//     // Use the console to output diagnostic information (console.log) and errors (console.error)
-//     // This line of code will only be executed once when your extension is activated
-//     console.log('Congratulations, your extension "alan-definitions" is now active!');
-
-//     // The command has been defined in the package.json file
-//     // Now provide the implementation of the command with  registerCommand
-//     // The commandId parameter must match the command field in package.json
-//     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-//         // The code you place here will be executed every time your command is executed
-
-//         // Display a message box to the user
-//         vscode.window.showInformationMessage('Hello World!');
-//     });
-
-//     context.subscriptions.push(disposable);
-// }
-
-// // this method is called when your extension is deactivated
-// export function deactivate() {
-// }
